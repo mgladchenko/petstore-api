3f8b80ce18e00856e77a71698cb30b968fb8109e
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2020-03-10 13:42:24 +0200

    COVID-19 first rest test

--- src/test/java/GetPetTest.java
+++ src/test/java/GetPetTest.java
@@ -1,6 +1,28 @@
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class GetPetTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Test
+    public void getPetById() {
+        int id = 0;
+        given()
+                .log()
+                .all()
+                .baseUri("https://petstore.swagger.io")
+                .when()
+                .get("/v2/pet/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(401);
     }
+
+    @Test
+    public void createPet() {
+
+    }
+
 }
